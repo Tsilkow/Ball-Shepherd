c37d603cb5b8b687e72174fb4ab067605cc4a266
Tom <tmsilkowski@gmail.com>
2022-12-14 23:20:40 +0100

    Task 3 finished Some problems with losing the ball, when centering towards middle of pillars

--- assignment_2_solution.py
+++ assignment_2_solution.py
@@ -5,6 +5,10 @@ import cv2
 import numpy as np
 
 
+def array_center(data):
+    return int(round(np.average(np.indices(data.shape)[0], weights=data)))
+
+
 def forward_distance(photo, return_center=False):
     focal_length = 311200
     buffor = 2000
@@ -13,16 +17,13 @@ def forward_distance(photo, return_center=False):
     mask_1 = cv2.inRange(hsv_photo, np.array([  0, 100,  50]), np.array([ 15, 255, 255]))
     mask_2 = cv2.inRange(hsv_photo, np.array([165, 100,  50]), np.array([179, 255, 255]))
     mask = mask_1 + mask_2
-    center = (np.argmax(np.sum(mask, axis=1)), np.argmax(np.sum(mask, axis=0)))
-    #print(f'center = {center}')
-    #mask[center[0], center[1]] = 0
-    #cv2.imshow('a', photo[:420, :])
-    #cv2.waitKey()
-    #cv2.destroyAllWindows()
     if np.sum(mask) == 0:
         if return_center: return None, None
         else: return None
-    
+    center = (array_center(np.sum(mask, axis=1)), array_center(np.sum(mask, axis=0)))
+    # print(f'center = {center}')
+    # photo[center[0], center[1]] = 0
+    # show_mask(photo)
     left  = center[1]
     right = center[1]
     #print(f'left, right={left, right}, {mask[left , center[1]]}')
@@ -40,33 +41,108 @@ def forward_distance(photo, return_center=False):
 def rotate_car(car, forward, clockwise=True):
     #print(f'forward={forward}, clockwise={clockwise}')
     #print(f'forward={forward}, steer={(forward*2-1) * (clockwise*2-1) * -1}')
-    drive(car, forward, (forward*2-1) * (clockwise*2-1) * -1)
+    drive(car, forward, (forward*2-1) * (clockwise*2-1))
     return not forward
 
 
 def find_a_ball(car):
-    distance_threshold = 140
+    distance_threshold = 130
     centerness_threshold = 50
     
     drive_forward = False
     distance = None
-    while distance == None or distance > distance_threshold:
+    while True:
         view = take_a_photo(car)
         #cv2.imshow('a', view)
         #cv2.waitKey()
-        #cv2.destroyAllWindows()
         distance, left_right = forward_distance(view, True)
         if distance == None:
             #print(f'No ball visible', end='\r')
             drive_forward = rotate_car(car, drive_forward)
-        else:
+        elif abs(left_right) > centerness_threshold:
             #print(f'Estimated distance={distance:5}, left_right={left_right:3}', end='\r')
-            if abs(left_right) < centerness_threshold: drive(car, True, 0)
-            else: rotate_car(car, False, (left_right > 0))
+            drive_forward = rotate_car(car, drive_forward, (left_right < 0))
+        elif distance > distance_threshold:
+            #print(f'Estimated distance={distance:5}, left_right={left_right:3}', end='\r')
+            drive(car, True, 0)
+        else: break
     #print('\n')
 
 
+def show(car):
+    photo = take_a_photo(car)
+    cv2.imshow('a', photo)
+    cv2.waitKey()
+
+
+def show_mask(mask):
+    cv2.imshow('a', mask)
+    cv2.waitKey()
+
+
+def find_pillars(view, color):
+    number_strips = 12
+    
+    hsv_view = cv2.cvtColor(view[:420, :], cv2.COLOR_RGB2HSV)
+    if color == 'green':
+        mask = cv2.inRange(hsv_view, np.array([ 45, 100,  50]), np.array([ 75, 255, 255]))
+    elif color == 'blue':
+        mask = cv2.inRange(hsv_view, np.array([110, 100,  50]), np.array([140, 255, 255]))
+    if np.amax(np.sum(mask, axis=1)) == 0: return None
+
+    strip_presence = []
+    strip_width = view.shape[1] // number_strips
+    divide = None
+    for x in range(0, view.shape[1], strip_width):
+        strip = mask[:, x:x+strip_width]
+        strip_presence.append(np.sum(strip))
+        if divide == None and strip_presence[-1] > 0 : divide = -1
+        elif divide == -1 and strip_presence[-1] == 0: divide = x+1
+    print(f'divide={divide} | {strip_presence}')
+    if divide == -1: return None
+    left_field  = np.sum(mask[:, :divide], axis=0)
+    right_field = np.sum(mask[:, divide:], axis=0)
+    if np.amax(left_field) == 0 or np.amax(right_field) == 0:
+        print(f'left_field={left_field} | right_field={right_field}')
+        show_mask(view)
+        return None
+    left_pillar  = array_center(left_field)
+    right_pillar = divide + array_center(right_field)
+    print(left_pillar, right_pillar)
+    view[:, divide] = [0, 0, 0, 255]
+    view[:, left_pillar] = [255, 0, 0, 255]
+    view[:, right_pillar] = [0, 255, 0, 255]
+    view[:, (left_pillar + right_pillar)//2] = [255, 255, 0, 255]
+    show_mask(view)
+    print(int(round((left_pillar + right_pillar)/2 - view.shape[1]/2)))
+    return int(round((left_pillar + right_pillar)/2 - view.shape[1]/2))
+
+
 def move_a_ball(car):
+    centerness_threshold = 50
+
+    #show(car)
+    find_a_ball(car)
+    for _ in range(5): drive(car, True, 0)
+    while True:
+        view = take_a_photo(car)
+        #show_mask(view)
+        if find_pillars(view, 'blue') is not None: break
+        drive(car, True, 1)
+        drive(car, True, 0)
+
+    #show(car)
+    while True:
+        view = take_a_photo(car)
+        left_right = find_pillars(view, 'blue')
+        print(f'blue pillars: {left_right}')
+        if left_right == None:
+            left_right = find_pillars(view, 'green')
+            print(f'green pillars: {left_right}')
+        if left_right == None: break
+        if abs(left_right) < centerness_threshold: drive(car, True, 0)
+        else: rotate_car(car, True, (left_right < 0))
+        #show(car)
     # TODO: you should write this function using
     # - take_a_photo(car)
     # - drive(car, forward, direction)
